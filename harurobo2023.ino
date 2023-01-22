6912f87ce6c413df694a39846ae90902cb6d8faf
mii <109723126+MiiGoto@users.noreply.github.com>
2023-01-23 07:50:39 +0900

    Update harurobo2023.ino

--- harurobo2023.ino
+++ harurobo2023.ino
@@ -2,6 +2,7 @@
 #include <FlexCAN.h>
 #include "PID.h"
 #include <math.h>
+#include <kinetis_flexcan.h>
 
 typedef struct
 {
@@ -19,7 +20,10 @@ Pid pid1;
 Pid pid2;
 Pid pid3;
 
+int flag = 0;
 float vx, vy, vt;
+int u[4] = {};
+int u2[4] = {};
 
 float rpm,mps;
 #define tire_dia 152//こういう感じに定数にしといても良いかも
@@ -35,15 +39,15 @@ float mps2rpm(float mps){//m/sをrpmに変換
 void setup(void)
 {
   CANTransmitter.begin();
+  pinMode(13, OUTPUT);
   Serial.begin(115200);
-  delay(1000);
+  Serial1.begin(10000, SERIAL_8E1);
+  pinMode(13,OUTPUT);
+  digitalWrite(13,HIGH);
+  delay(2000);
+  digitalWrite(13,LOW);  
 
-  msg.len = 8;
-  msg.id = 0x200;
-  for ( int idx = 0; idx < msg.len; ++idx ) {
-    msg.buf[idx] = 0;
-  }
-
+  
   pid0.init(3.0, 0.001, 0.03); //p,i,dの順に指定できる
   pid1.init(3.0, 0.001, 0.03);
   pid2.init(3.0, 0.001, 0.03);
@@ -54,9 +58,73 @@ void setup(void)
 
 }
 int cnt=0;
-
+static unsigned long testch[6];
 void loop(void)
 {
+    static int data[18];                      //入力データが入る？
+  static int dataNumber = 0;                //入力データの数(Serial1.available()の返値)
+  static unsigned long lastConnectTime = 0; //直前の通信の時間?
+  if (Serial1.available() > 0) {
+    for (int dataNum = Serial1.available(); dataNum > 0; dataNum--) {
+      if (dataNumber < 0) {
+        Serial1.read();
+        dataNumber++;
+        continue;
+      }
+      data[dataNumber % 18] = Serial1.read();
+      dataNumber++;
+      if (dataNumber > 18) {
+        dataNumber = 0;
+      }
+      else if (dataNumber == 18) {
+        testch[0] = (((data[1] & 0x07) << 8) | data[0]);          //ch0(364～1024～1684)
+        testch[1] = (((data[2] & 0x3F) << 5) | (data[1] >> 3));   //ch1(364～1024～1684)
+        testch[2] = (((data[4] & 0x01) << 10) | (data[3] << 2) | (data[2] >> 6)); //ch2(364～1024～1684)
+        testch[3] = (((data[5] & 0x0F) << 7) | (data[4] >> 1));   //ch3(364～1024～1684)
+        if (!(364 <= testch[0] && testch[0] <= 1684 && 364 <= testch[1] && testch[1] <= 1684 && 364 <= testch[2] && testch[2] <= 1684 && 364 <= testch[3] && testch[3] <= 1684)) {
+          for (int i = 1; i < 18; i++) {
+            testch[0] = (((data[(1 + i) % 18] & 0x07) << 8) | data[(0 + i) % 18]);  //ch0(364～1024～1684)
+            testch[1] = (((data[(2 + i) % 18] & 0x3F) << 5) | (data[(1 + i) % 18] >> 3)); //ch1(364～1024～1684)
+            testch[2] = (((data[(4 + i) % 18] & 0x01) << 10) | (data[(3 + i) % 18] << 2) | (data[2] >> 6)); //ch2(364～1024～1684)
+            testch[3] = (((data[(5 + i) % 18] & 0x0F) << 7) | (data[(4 + i) % 18] >> 1)); //ch3(364～1024～1684)
+            if (364 <= testch[0] && testch[0] <= 1684 && 364 <= testch[1] && testch[1] <= 1684 && 364 <= testch[2] && testch[2] <= 1684 && 364 <= testch[3] && testch[3] <= 1684) {
+              dataNumber = -i;
+              break;
+            }
+          }
+          if (dataNumber > 18) {
+            dataNumber = -1;
+          }
+        }
+        else {
+          dataNumber = 0;
+        }
+      }
+    }
+    digitalWrite(13, !digitalRead(13)); //プロポ受信したらLEDチカチカ
+    flag = 1;
+  }
+  else {
+    flag = 0;
+  }
+    
+  
+
+  if (((data[5] & 0xC0) >> 6) == 1) {
+    m_msg.id = 0x200;
+    m_msg.len = 8;
+    for (int i = 0; i < 4; i++) {
+      u[i] = 0;
+    }
+    if (testch[3] >= 364 && testch[3] <= 1684) {
+      vy = map(testch[3], 364, 1684, -5000, 5000);//目標最大rpmは5000
+    }
+    if (testch[2] >= 364 && testch[2] <= 1684) {
+      vx = map(testch[2], 364, 1684, -5000, 5000);
+    }
+    if (testch[0] >= 364 && testch[0] <= 1684) {
+      vt = map(testch[0], 364, 1684, -5000, 5000);
+    }
   int u[4] = {0};
   //u[0] = 500;
   //u[1] = 500;
@@ -84,10 +152,10 @@ void loop(void)
   u[2] = pid2.pid_out(u[2]);
   u[3] = pid3.pid_out(u[3]);
   
-    u[0] = (int)(min(max(-16000, -vx - vy + vt), 16000));
-    u[1] = (int)(min(max(-16000, -vx + vy + vt), 16000));
-    u[2] = (int)(min(max(-16000, vx + vy + vt), 16000));
-    u[3] = (int)(min(max(-16000, vx - vy + vt), 16000)); 
+  u[0] = (int)(min(max(-16000, -vx - vy + vt), 16000));
+  u[1] = (int)(min(max(-16000, -vx + vy + vt), 16000));
+  u[2] = (int)(min(max(-16000, vx + vy + vt), 16000));
+  u[3] = (int)(min(max(-16000, vx - vy + vt), 16000)); 
    
   Serial.println(u[0]);
   Serial.println(u[1]);
@@ -96,8 +164,21 @@ void loop(void)
 
    for (int i = 0; i < 4; i++) {
     msg.buf[i * 2] = u[i] >> 8;
-    msg.buf[i * 2 + 1] = u[i] & 0xFF;
+    msg.buf[i * 2 + 1] = u[i] & 0xFF;    
   }
+  for (int i = 0; i < gm_msg.len; i++) {
+      gm_msg.buf[i * 2] = u[i] >> 8;
+      gm_msg.buf[i * 2 + 1] = u[i] & 0xFF;
+    }
+    analogWrite(23, 0);
+    analogWrite(21, 0);
+  }
+  if(digitalRead(2)==1){
+    analogWrite(23, 0);
+    analogWrite(21, 0);
+  }
+  delay(50);
+
   //Serial.print(pid0.debug());//現在速度
   //Serial.print(",");
   //Serial.print(pid1.debug());
@@ -106,7 +187,6 @@ void loop(void)
   //Serial.print(",");
   //Serial.print(pid3.debug());
   //Serial.println("");
-  delay(10); //シリアル通信が終わる前に次の通信が始まってしまうのを防ぐ
 }
 
 void timerInt() {
